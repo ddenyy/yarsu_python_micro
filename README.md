dbf967e256264811138b6006d39aa5deb00fcfb5
ssmess <otboeva.pmi@gmail.com>
2025-04-19 20:00:23 +0300

    folder add, files delete

--- README.md
+++
@@ -1,2 +0,0 @@
-# yarsu_python_micro
-students create a base services architecture
---
+++ schedule_service/schemas.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+import enum
+
+class LessonType(str, enum.Enum):
+    lecture = "lecture"
+    practice = "practice"
+
+class LessonCreate(BaseModel):
+    course_name: str
+    teacher_id: int
+    group_name: str
+    start_time: datetime
+    end_time: datetime
+    room_id: int
+    lesson_type: LessonType
+
+class LessonUpdate(BaseModel):
+    course_name: Optional[str]
+    teacher_id: Optional[int]
+    group_name: Optional[str]
+    start_time: Optional[datetime]
+    end_time: Optional[datetime]
+    room_id: Optional[int]
+    lesson_type: Optional[LessonType]
+
+class LessonOut(BaseModel):
+    id: int
+    course_name: str
+    teacher_id: int
+    group_name: str
+    start_time: datetime
+    end_time: datetime
+    room_id: int
+    lesson_type: LessonType
+
+    class Config:
+        orm_mode = True
+
